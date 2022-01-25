984bf7aad6c12853bfb55114fcda86520cd61fad
Stephanie Nuñez <nunezcorrea.stephanie@gmail.com>
2022-01-25 17:18:25 -0500

    Add methods to products controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :products, only: [ :index, :new, :create ]
+  resources :products do
+    resources :booking_requests, only: [ :new, :create ]
+  end
+  resources :booking_requests, only: [ :index, :accept, :reject ]
 end
