0f8f8f3b17896479ef1de0de082d66d61d51d24b
Stephanie Nuñez <nunezcorrea.stephanie@gmail.com>
2022-01-25 11:23:45 -0500

    add product model

---
+++ db/migrate/20220125162233_create_products.rb
@@ -0,0 +1,15 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.string :address
+      t.boolean :is_available
+      t.references :user, null: false, foreign_key: true
+      t.string :city
+      t.float :price
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_225130) do
+ActiveRecord::Schema.define(version: 2022_01_25_162233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "address"
+    t.boolean "is_available"
+    t.bigint "user_id", null: false
+    t.string "city"
+    t.float "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +40,5 @@ ActiveRecord::Schema.define(version: 2022_01_24_225130) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "users"
 end
