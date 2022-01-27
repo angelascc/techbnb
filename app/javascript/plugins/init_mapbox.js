b88343395fcb9da60a1b850a93a1c53751d5c810
Stephanie Nuñez <nunezcorrea.stephanie@gmail.com>
2022-01-27 14:38:43 -0500

    add search

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,6 +1,12 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
@@ -11,6 +17,15 @@ const initMapbox = () => {
       style: 'mapbox://styles/mapbox/streets-v10'
     });
   }
+
+  const markers = JSON.parse(mapElement.dataset.markers);
+  markers.forEach((marker) => {
+    new mapboxgl.Marker()
+      .setLngLat([marker.lng, marker.lat])
+      .addTo(map);
+  });
+
+  fitMapToMarkers(map, markers);
 };
 
 export { initMapbox };
