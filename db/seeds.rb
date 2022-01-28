69650df2bfd0786e5e142907b58e798a5ad94d62
angela cuadros <angelascc16@gmail.com>
2022-01-27 19:22:21 -0500

    final version 3.0

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,21 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
-
-puts 'Creating 10 fake products...'
-10.times do
-  product = Product.new(
-    name: Faker::Commerce.product_name,
-    description: Faker::Commerce.material,
-    address: Faker::Address.street_address,
-    city: Faker::Address.city,
-    price: Faker::Commerce.price
-  )
-  product.save!
-end
-
-puts 'Finished!'
 
 # "#{Faker::Address.street_address}, #{Faker::Address.city}",
 # rating: rand(0..5)
